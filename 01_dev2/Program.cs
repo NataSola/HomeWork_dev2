6f43aff90b9aa8dcf80b5c5609ff98e846ef0a93
NataSola <nsol2009@yandex.ru>
2021-11-05 00:44:15 +0300

    Add Array and selection in ascending order

--- 01_dev2/Program.cs
+++ 01_dev2/Program.cs
@@ -7,28 +7,43 @@
 //Console.ReadLine()
 
 
-int n = 25;
-int[] arrayA = new int[n];
+int n = 20;                                         // размер массива
+int[] arrayA = new int[n];                          // объявление массива
 
 
-void FillArray(int[] array)
+void FillArray(int[] array)                         // метод заполнения массива
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)                     // количество элементов n, для каждого элемента:
     {
-        arrayA[i] = new Random().Next(-100, 101);
+        arrayA[i] = new Random().Next(-100, 101);   // выбирается случайное число из диапазона от -100 до 100 включительно
     }
 }
 
-void PrintArray(int[] arr)
+void PrintArray(int[] arr)                          // метод для вывода массива на печать
 {
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]} ");
     }
-    Console.WriteLine();Console.WriteLine();
+    Console.WriteLine(); Console.WriteLine();
 }
 
-
 FillArray(arrayA);
-Console.WriteLine();Console.WriteLine($"Массив A из {(n)} случайных чисел из диапазона от -100 до 100 включительно:");
+Console.WriteLine(); Console.WriteLine($"Массив A из {(n)} случайных чисел из диапазона от -100 до 100 включительно:");
 PrintArray(arrayA);
+
+// Элементы массива А, не нарушающие порядок чередования
+
+int current = arrayA[0];
+System.Console.WriteLine("Элементы массива А, не нарушающие порядок чередования:");
+System.Console.Write($"{current} ");
+for (int i = 1; i < n; i++)
+{
+    if (arrayA[i] > current)
+    {
+        current = arrayA[i];
+        System.Console.Write(current + " ");
+    }
+}
+Console.WriteLine(); Console.WriteLine();
+
